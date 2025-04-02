eac1871ee115b5e88f785ff6ce6132dc16061a24
Nate Silva <nate@natesilva.com>
2025-04-02 11:43:20 -0700

    feat: add a debug log message

--- bin/create-typescript.js
+++ bin/create-typescript.js
@@ -10,6 +10,8 @@ import { installEslint } from "../lib/install-eslint.js";
 import { installPrettier } from "../lib/install-prettier.js";
 import { installVitest } from "../lib/install-vitest.js";
 
+console.log(process.argv);
+
 const basePath = fileURLToPath(import.meta.url);
 const starterFilesPath = resolve(basePath, "..", "..", "starter-files");
 
